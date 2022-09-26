9ac3a99ee499a7d4bca60b99347bf143e8b3bae4
loomz <loomz@163.com>
2022-09-26 15:36:25 +0800

    更新代码

--- pytest_wm/ddt/TestEnorll_json.py
+++ pytest_wm/ddt/TestEnorll_json.py
@@ -2,20 +2,19 @@ import json
 import pytest
 
 
-class TestEnorll():
-    def get_data(self):
-        """
-        ：读取json文件
-        """
-        data = []
-        with open(self, 'r') as f:
-            dict_data = json.load(f.read())
-            for i in dict_data:
-                data.append(tuple(i.values()))
+def get_data_json(file_json):
+    with open(file_json, encoding="utf-8") as f:
+        lit = []
+        keys = json.load(f)
+        for key in keys:
+            lit.append(tuple(key.values()))
+        return lit
 
-    @pytest.mark.parametrize("name,email,pwd,pwd_", get_data(BASE_DIR + '\\data' + '\\enrolls.json'),
-                             ids=['注册成功', '密码不一致', '会员输入框为空', '重命名', '电子邮箱为空', '电子邮箱错误', '密码输入框为空']
-                             )
-    def test_case(self, browser, name, email, pwd, pwd_, url=None):
-        obj = Enorrllpage(browser)
-        pass
+
+@pytest.mark.parametrize("name,email,pwd,pwd_", get_data_json('testE.json'))
+def test_case( name, email, pwd, pwd_):
+    assert pwd == pwd_
+
+
+if __name__ == '__main__':
+    pytest.main(['-sv', 'testEnorll_json.py'])
